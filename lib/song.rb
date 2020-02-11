1f661cd84111ae2a7cb30989c3da689754e48a49
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-02-11 03:23:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,7 @@ def self.create
   end
   
   def self.find_or_create_by_name(name)
-    self.create_by_name(name) || self.find_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
   end
   
 end
